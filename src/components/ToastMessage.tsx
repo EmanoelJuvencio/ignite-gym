c72af29d661ffe3ec038f00448a1e15dc81f8a33
Emanoel <emanoeljuvencio@gmail.com>
2024-09-06 17:19:33 -0300

    feat: Criando Componente de ToastMessage

---
+++ src/components/ToastMessage.tsx
@@ -0,0 +1,62 @@
+import {
+  Box,
+  Icon,
+  Pressable,
+  Toast,
+  ToastDescription,
+  ToastTitle,
+} from '@gluestack-ui/themed'
+import { XCircle } from 'lucide-react-native'
+
+type TToastMessageProps = {
+  id: string
+  title: string
+  description?: string
+  action?: 'error' | 'success'
+  onClose: () => void
+}
+
+export function ToastMessage({
+  id,
+  title,
+  description,
+  action = 'success',
+  onClose,
+}: TToastMessageProps) {
+  return (
+    <Toast
+      nativeID={`toast-${id}`}
+      action={action}
+      bg={action === 'success' ? '$green500' : '$red500'}
+      mt='$10'
+      flexDirection='column'
+    >
+      <Box
+        flexDirection='row'
+        gap={10}
+        justifyContent='space-between'
+        alignItems='center'
+      >
+        <ToastTitle
+          flex={1}
+          numberOfLines={2}
+          color='$white'
+          fontFamily='$heading'
+        >
+          {title}
+        </ToastTitle>
+        <Pressable alignItems='center' onPress={onClose}>
+          <Icon as={XCircle} color='$coolGray50' size='lg' />
+        </Pressable>
+      </Box>
+
+      {description && (
+        <Box mt='$2' pt='$4' borderTopWidth={0.2} borderColor='$coolGray100'>
+          <ToastDescription color='$white' fontFamily='$body'>
+            {description}
+          </ToastDescription>
+        </Box>
+      )}
+    </Toast>
+  )
+}
