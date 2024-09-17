1146f936dab7364af30a0b6b4c4d9804c7d0c286
Emanoel <emanoeljuvencio@gmail.com>
2024-09-16 22:37:50 -0300

    feat: criando e consumindo hook useAuth

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,9 +1,8 @@
-import { useContext } from 'react'
 import { Box } from '@gluestack-ui/themed'
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native'
 import { gluestackUIConfig } from '../../config/gluestack-ui.config'
 
-import { AuthContext } from '@contexts/AuthContext'
+import { useAuth } from '@hooks/useAuth'
 
 import { AuthRoutes } from './auth.routes'
 // import { AppRoutes } from './app.routes'
@@ -12,8 +11,9 @@ export function Routes() {
   const theme = DefaultTheme
   theme.colors.background = gluestackUIConfig.tokens.colors.gray700
 
-  const authContext = useContext(AuthContext)
-  console.log(authContext)
+  const { user } = useAuth()
+
+  console.log('User vindo do hook useAuth ==>', user)
 
   return (
     <Box flex={1} bg='$gray700'>
